06949fab3ece799970aa84917ff8f6af9cfd5496
sinsha <sinshasb@gmail.com>
2024-04-02 21:26:44 +0530

    pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+// App.js
+
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Favourite from "./pages/Favourite";
 
 function App() {
   return (
-   <>
-  Private contact
-   </>
+    <Router>
+      <div>private contacts</div>
+      <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/favourite">Favourite</Link>
+          </li>
+         
+        </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+    </Router>
   );
 }
 
